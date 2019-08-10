15dd10ed9e21d15efbc3cf18a6b74ef215b5d5d1
Olja Ivkovic <olja.ivkovic97@gmail.com>
2019-08-10 19:51:48 +0200

    create user migration

---
+++ priv/repo/migrations/20190810174059_add_user_table.exs
@@ -0,0 +1,14 @@
+defmodule PersonalWeather.Repo.Migrations.AddUserTable do
+  use Ecto.Migration
+
+  def change do
+    create table :user do
+      add :first_name, :string, null: false, size: 50
+      add :last_name, :string, null: false, size: 50
+      add :email, :string, null: false, size: 100
+      add :password_hash, :string, null: false
+      add :is_active, :boolean, null: false, default: false
+      timestamps()
+    end
+  end
+end
